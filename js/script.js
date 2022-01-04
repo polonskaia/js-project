6594ce13c807e5440dc8a82cd7b6c3310814eda3
Olga Polonskaia <necheuhina.olya@gmail.com>
2022-01-04 19:00:13 +0200

    added the function of creating a contact

--- js/script.js
+++ js/script.js
@@ -4,25 +4,73 @@ document.addEventListener('DOMContentLoaded', () => {
   const cancelAddButton = document.getElementById('add-new-client__cancel-btn');
   const addNewClientModalContainer = document.getElementById('add-new-client');
   const addNewClientModal = document.getElementById('add-new-client__modal');
+  const addContactContainer = document.getElementById('add-contact-btn__container');
+  const addContactBtn = document.getElementById('add-contact__btn');
+  const contactsList = ['Телефон', 'Доп. телефон', 'Email', 'Vk', 'Facebook', 'Другое'];
+  const saveNewClientBtn = document.getElementById('add-new-client__save-btn');
 
   addClientButton.addEventListener('click', () => {
-    addNewClientModalContainer.classList.add('modal-visible');
+    addNewClientModalContainer.classList.add('visible');
   });
 
   closeNewClientFormButton.addEventListener('click', () => {
-    addNewClientModalContainer.classList.remove('modal-visible');
+    addNewClientModalContainer.classList.remove('visible');
   });
 
   cancelAddButton.addEventListener('click', () => {
-    addNewClientModalContainer.classList.remove('modal-visible');
+    addNewClientModalContainer.classList.remove('visible');
   });
 
   addNewClientModalContainer.addEventListener('click', () => {
-    addNewClientModalContainer.classList.remove('modal-visible');
+    addNewClientModalContainer.classList.remove('visible');
   });
 
   addNewClientModal.addEventListener('click', (event) => {
     event.stopPropagation();
   });
 
+  addContactBtn.addEventListener('click', () => {
+    createContactSelect(addContactContainer, contactsList);
+  });
+
+  saveNewClientBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    getOptionValue();
+    addNewClientModalContainer.classList.remove('visible');
+  });
+
+  function createContactSelect(container, list) {
+    const selectAndInputContainer = document.createElement('div');
+    selectAndInputContainer.classList.add('contact-container');
+    container.insertBefore(selectAndInputContainer, addContactBtn);
+
+    const contactSelect = document.createElement('select');
+    contactSelect.classList.add('selectCustom')
+    selectAndInputContainer.appendChild(contactSelect);
+
+    list.forEach((item) => {
+      const contactOption = document.createElement('option');
+      contactOption.innerHTML = item;
+      contactOption.setAttribute('value', item);
+      contactSelect.appendChild(contactOption);
+    });
+
+    const contactInput = document.createElement('input');
+    contactInput.classList.add('contact-input');
+    selectAndInputContainer.appendChild(contactInput);
+    contactInput.placeholder = 'Введите данные контакта';
+
+    const deleteContactBtn = document.createElement('button');
+    deleteContactBtn.classList.add('delete-contact-btn', 'btn');
+    deleteContactBtn.type = 'button';
+    selectAndInputContainer.appendChild(deleteContactBtn);
+  }
+
+  function getOptionValue() {
+    const selects = document.querySelectorAll('.selectCustom');
+    selects.forEach((s) => {
+      const optionValue = s.options[s.selectedIndex].value;
+      console.log(optionValue);
+    });
+  }
 });
