825ce38ab3ee26b8dac7bd8e1f664a61c638bce3
Olga Polonskaia <necheuhina.olya@gmail.com>
2022-01-14 21:02:05 +0200

    added function of creating table

--- js/script.js
+++ js/script.js
@@ -6,8 +6,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const addNewClientModal = document.getElementById('add-new-client__modal');
   const addContactContainer = document.getElementById('add-contact-btn__container');
   const addContactBtn = document.getElementById('add-contact__btn');
-  const contactsList = ['Телефон', 'Доп. телефон', 'Email', 'Vk', 'Facebook', 'Другое'];
   const saveNewClientBtn = document.getElementById('add-new-client__save-btn');
+  const tableBody = document.querySelector('.table__tbody');
+  const contactsList = ['Телефон', 'Доп. телефон', 'Email', 'Vk', 'Facebook', 'Другое'];
 
   addClientButton.addEventListener('click', () => {
     addNewClientModalContainer.classList.add('visible');
@@ -73,4 +74,61 @@ document.addEventListener('DOMContentLoaded', () => {
       console.log(optionValue);
     });
   }
+
+  function createTable(ID, fullName, creatingDate, updatingDate, contacts) {
+    const crDate = new Date(creatingDate);
+    const updDate = new Date(updatingDate);
+
+    const row = document.createElement('tr');
+    tableBody.append(row);
+
+    row.innerHTML = `
+    <td>${ID}</td>
+    <td>${fullName}</td>
+    <td>${crDate}</td>
+    <td>${updDate}</td>
+    <td>...</td>
+    <td>Изменить</td>
+    <td>Удалить</td>
+    `;
+  }
+
+  function addClassesToTableCells() {
+    const tdList = document.getElementsByTagName('td');
+    const tdArray = Array.from(tdList);
+
+    tdArray.forEach((td) => {
+      td.classList.add('table__td');
+    });
+
+    const trArray = Array.from(document.getElementsByTagName('tr')).slice(1);
+
+    trArray.forEach((row) => {
+      const firstCells = Array.from(row.getElementsByTagName('td'))[0];
+      firstCells.classList.add('table__td_id');
+    });
+  }
+
+  async function getClientsFromServer() {
+    const clients = await fetch('http://localhost:3000/api/clients');
+    const clientsData = await clients.json();
+    return clientsData;
+  }
+
+  (async () => {
+    const clientsList = await getClientsFromServer();
+    console.log(clientsList);
+
+    clientsList.forEach((client) => {
+      const id = client.id;
+      const fname = [client.surname, client.name, client.lastName].join(' ');
+      const create = client.createdAt;
+      const update = client.updatedAt;
+      const contactsList = client.contacts;
+      createTable(id, fname, create, update, contactsList);
+    });
+
+    addClassesToTableCells();
+  })();
+
 });
